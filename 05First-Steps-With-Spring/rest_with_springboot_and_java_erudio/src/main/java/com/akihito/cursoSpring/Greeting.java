8a786112e77e00024e02e51063827a8d66371109
akihito7 <akihitopro7@gmail.com>
2023-05-17 13:56:01 -0300

    primeiro end point com spring

---
+++ 05First-Steps-With-Spring/rest_with_springboot_and_java_erudio/src/main/java/com/akihito/cursoSpring/Greeting.java
@@ -0,0 +1,25 @@
+package com.akihito.cursoSpring;
+
+public class Greeting {
+  private final long id ;
+  private final String name;
+  
+  
+  public Greeting(long id, String name) {
+	  this.id = id;
+	  this.name = name;
+	  
+  }
+
+
+public long getId() {
+	return id;
+}
+
+
+public String getName() {
+	return name;
+}
+  
+  
+}
